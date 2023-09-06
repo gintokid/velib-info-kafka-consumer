443be575de98f63a78eb9f3b67cb74d4a2fd0cc6
gintokid <juventino1802@gmail.com>
2023-09-06 22:41:49 +0200

    fix erreurs de package

--- src/main/java/com/madroid/velibdataconsume/config/KafkaConsumerConfig.java
+++ src/main/java/com/madroid/velibdataconsume/config/KafkaConsumerConfig.java
@@ -1,12 +1,9 @@
 package com.madroid.velibdataconsume.config;
 
-import com.madroid.velibdata.config.JpaConfiguration;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
